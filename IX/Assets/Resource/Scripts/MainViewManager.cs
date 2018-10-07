ff063e84ed72dc813d12d68d1ca69bbd292c60f9
BlackWings1937 <36805728+BlackWings1937@users.noreply.github.com>
2018-10-07 23:47:10 +0800

    testScriptUpdate
    testScriptUpdate

---
+++ IX/Assets/Resource/Scripts/MainViewManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainViewManager : MonoBehaviour {
+
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        if (text!= null) {
+            text.text = "你好，老大~";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
